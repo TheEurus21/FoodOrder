2c27bb5eb089ed6a10db025a28f0de089553a8d6
Saba <sabaa.skzz@gmail.com>
2025-12-03 11:34:49 +0330

    CommonRepo

---
+++ FoodOrder/Repositories/Common/ApplicationRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FoodOrder.Data;
+namespace FoodOrder.Repositories.Common
+{
+    public class ApplicationRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public ApplicationRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task<List<T>>GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+        public async Task<T>GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task<T>AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+
+        }
+        public async Task<T>UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null) return false;
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+     
+    }
+
+}
