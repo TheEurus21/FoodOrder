8facf880f471e89fb7533aacb57175ef46572b58
Saba <sabaa.skzz@gmail.com>
2025-12-03 10:14:27 +0330

    updateController

--- FoodOrder/Controllers/RestaurantController.cs
+++ FoodOrder/Controllers/RestaurantController.cs
@@ -30,7 +30,53 @@ namespace FoodOrder.Controllers
             return restaurants.Select(MapToResponse).ToList();
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RestaurantResponse>>GetById(int id)
+        {
+            var restaurant =await _context.Restaurants
+                .Include(r => r.Categories)
+                .ThenInclude(c=>c.Foods)
+                .FirstOrDefaultAsync(r => r.Id == id);
+            if (restaurant == null) return NotFound();
+            return MapToResponse(restaurant);
+        }
+        [HttpPost]
+        public async Task<ActionResult<RestaurantResponse>>Create(Restaurant restaurant)
+        {
+            restaurant.RestaurantCode = Guid.NewGuid();
 
+            _context.Restaurants.Add(restaurant);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new {id=restaurant.Id},MapToResponse(restaurant));
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Restaurant restaurant)
+        {
+            if (id != restaurant.Id) return BadRequest();
+            _context.Entry(restaurant).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RestaurantExists(id)) return NotFound();
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult>Delete(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null) return NotFound();
+            _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool RestaurantExists(int id)
+        {
+            return _context.Restaurants.Any(e => e.Id == id);
+        }
 
         private RestaurantResponse MapToResponse(Restaurant restaurant)
         {
