9c8fc8fef0234c348ba278467bbfb34bb68c82a9
Saba <sabaa.skzz@gmail.com>
2025-12-07 15:54:56 +0330

    addedd created at and updated at

--- FoodOrder/Models/FoodCategory.cs
+++ FoodOrder/Models/FoodCategory.cs
@@ -3,11 +3,24 @@
     public class FoodCategory
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public int RestaurantId {  get; set; }
-        public Restaurant Restaurant { get; set; }
-        public ICollection<Food> Foods { get; set; }=new List<Food>();
 
+
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+
+        public int RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+
+  
+        public ICollection<Food> Foods { get; set; } = new List<Food>();
+
+   
+        public int UserId { get; set; }          
+        public User User { get; set; }           
+
+  
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
     }
 }
