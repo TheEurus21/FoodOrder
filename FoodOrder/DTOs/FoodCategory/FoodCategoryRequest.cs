016413fb3ba51b4cebe966a38b3811c4d7f934c6
Saba <sabaa.skzz@gmail.com>
2025-12-08 15:38:29 +0330

    food category request and response

--- FoodOrder/DTOs/FoodCategory/FoodCategoryRequest.cs
+++ FoodOrder/DTOs/FoodCategory/FoodCategoryRequest.cs
@@ -5,7 +5,6 @@ namespace FoodOrder.DTOs.FoodCategory
     public class FoodCategoryRequest
     {
         public string? Name { get; set; }
-        public string? Description { get; set; }
         public List<FoodRequest> Foods { get; set; } = new();
 
     }
--- FoodOrder/DTOs/FoodCategory/FoodCategoryResponse.cs
+++ FoodOrder/DTOs/FoodCategory/FoodCategoryResponse.cs
@@ -5,7 +5,6 @@ namespace FoodOrder.DTOs.FoodCategory
     public class FoodCategoryResponse
     {
         public string Name { get; set; }
-        public string Description { get; set; }
         public List<FoodResponse> Foods { get; set; } = new();
     }
 }
