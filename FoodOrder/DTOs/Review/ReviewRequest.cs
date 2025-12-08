8309bdd631368356aede9aaa5b5d85c4eaea9ed7
Saba <sabaa.skzz@gmail.com>
2025-12-08 15:37:55 +0330

    review request and review response dtos

---
+++ FoodOrder/DTOs/Review/ReviewRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodOrder.DTOs.Review
+{
+    public class ReviewRequest
+    {
+        public string FoodName { get; set; }         
+        public int Rating { get; set; }          
+        public string? Comment { get; set; }     
+    }
+}
---
+++ FoodOrder/DTOs/Review/ReviewResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodOrder.DTOs.Review
+{
+    public class ReviewResponse
+    {
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public int RestaurantId { get; set; }
+        public int? FoodId { get; set; }
+    }
+}
