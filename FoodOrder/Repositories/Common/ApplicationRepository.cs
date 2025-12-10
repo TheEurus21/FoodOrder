2ef38effd641db25e2c79713af93e853163074b0
Saba <sabaa.skzz@gmail.com>
2025-12-10 11:57:09 +0330

    new Tasks

--- FoodOrder/Repositories/Common/ApplicationRepository.cs
+++ FoodOrder/Repositories/Common/ApplicationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using FoodOrder.Data;
+using FoodOrder.Models;
 namespace FoodOrder.Repositories.Common
 {
     public class ApplicationRepository<T> where T : class
@@ -41,7 +42,51 @@ namespace FoodOrder.Repositories.Common
             await _context.SaveChangesAsync();
             return true;
         }
-     
+        public async Task<FoodCategory?> GetByOwnerIdAsync(int ownerId)
+        {
+            return await _context.Categories
+                                 .FirstOrDefaultAsync(c => c.UserId == ownerId);
+        }
+        public async Task<Food?> GetFoodByNameAndOwnerAsync(string FoodName, int ownerId)
+        {
+            return await _context.Foods
+                .Include(f => f.Category)
+                .ThenInclude(c => c.Restaurant)
+                .FirstOrDefaultAsync(f => f.Name == FoodName && f.Category.Restaurant.UserId == ownerId);
+        }
+        public async Task<Restaurant?> GetByNameAndOwnerAsync(string restaurantName, int ownerId)
+        {
+            return await _context.Restaurants
+                .FirstOrDefaultAsync(r => r.UserId == ownerId && r.Name == restaurantName);
+        }
+
+        public async Task<Order?> GetLatestOrderByUserAsync(int userId)
+        {
+            return await _context.Orders
+                .Where(o => o.UserId == userId)
+                .OrderByDescending(o => o.CreatedAt)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<Review?> GetLatestReviewByUserAsync(int userId)
+        {
+            return await _context.Reviews
+                .Where(r => r.UserId == userId)
+                .OrderByDescending(r => r.CreatedAt)
+                .FirstOrDefaultAsync();
+        }
+        public async Task<Review?> GetByRestaurantNameAndUserAsync(string restaurantName, int userId)
+        {
+            var restaurant = await _context.Restaurants
+                .FirstOrDefaultAsync(r => r.Name == restaurantName && r.UserId == userId);
+
+            if (restaurant == null) return null;
+
+            return await _context.Reviews
+                .FirstOrDefaultAsync(rv => rv.UserId == userId && rv.RestaurantId == restaurant.Id);
+        }
+
+
     }
 
 }
